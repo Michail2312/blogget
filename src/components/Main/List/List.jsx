13a5e77cb61d81a958edaaab35c15d24303378c1
Михаил Маковчук <krasnodar2312@mail.ru>
2023-03-12 12:45:43 +0300

    fix

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -5,32 +5,7 @@ import style from "./List.module.css";
 
 export const List = () => {
   const [postsBest] = useContext(postsContext);
-  // const postsData = [
-  //   {
-  //     title: "Title1",
-  //     author: "NICK NAME",
-  //     ups: 77,
-  //     date: "2023-03-04T19:09:00.000",
-  //   },
-  //   {
-  //     title: "Title2",
-  //     author: "NICK NAME",
-  //     ups: 58,
-  //     date: "2023-03-04T19:09:00.000",
-  //   },
-  //   {
-  //     title: "Title3",
-  //     author: "NICK NAME",
-  //     ups: 24,
-  //     date: "2023-03-04T19:09:00.000",
-  //   },
-  //   {
-  //     title: "Title4",
-  //     author: "NICK NAME",
-  //     ups: 124,
-  //     date: "2023-03-04T19:09:00.000",
-  //   },
-  // ];
+
   return (
     <ul className={style.list}>
       {postsBest.map((item) => (
